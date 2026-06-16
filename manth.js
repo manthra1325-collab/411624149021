d4791f0804e06b61d4f10f3fb81dc5a282669e37
Mini Prabhakaran <manthra1325@gmail.com>
2026-06-16 11:01:04 +0530

    JS

---
+++ manth.js
@@ -0,0 +1,66 @@
+// console.log("Manthra")
+
+// const a=20;
+// const b=80;
+// console.log(a+b)
+
+// const str ="Harini"
+// console.log(str)
+
+// const arr=[1,2.4,"manthra", false]
+// console.log(arr)
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i])
+// }
+
+// arr.push("Mayuthra")
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// function example()
+// {
+//     console.log("Hello everyone this is my js")
+// }example()
+
+// const ptag=document.getElementById("ptag")
+// ptag.innerHTML ="Manthravarthini.A.P"
+
+// const input =document.getElementById("input")
+// const  output =document.getElementById("inputField")
+// input.addEventListener("input",()=>{
+//     output.textContent=input.value})
+
+
+//     const first=document.createElement("h")
+//     first.textContent="Helloooo"
+//     document.body.appendChild(first)
+
+//     function demo(){
+//         console.log("Hello");
+//   }
+
+
+// function show() {
+//     document.getElementById("demo").innerHTML = "Hello World!";
+// }
+// 
+
+// function findSum() {
+//     let a = Number(document.getElementById("n1").value);
+//     let b = Number(document.getElementById("n2").value);
+//     let sum = a + b;
+//     document.getElementById("result").innerHTML = "Sum = " + sum;
+// }
+
+
+// function addNumbers() {
+//     let n1 = Number(document.getElementById("num1").value);
+//     let n2 = Number(document.getElementById("num2").value);
+
+//     let sum = n1 + n2;
+
+//     document.getElementById("result").innerHTML = "Sum = " + sum;
+// }
--- manu.js
+++ manu.js
@@ -1,41 +1,38 @@
-console.log("Manthra")
+console.log("A")
+console.log("B")
+console.log("c")
 
-const a=20;
-const b=80;
-console.log(a+b)
+// const promise= new Promise((resolve,reject)=>{
+    // synchronous 
+    // const success = true;
+    // if(success){
+    //     console.log("Done")
+    // }
+    // else{
+    //     console.log("No")
+    // }
 
-const str ="Harini"
-console.log(str)
+//     asynchronous 
+// setTimeout(()=>{
+//         console.log("hi")
+//         console.log("Over")
+// },2000)
+// console.log("O")
+// })
 
-const arr=[1,2.4,"manthra", false]
-console.log(arr)
 
-for(let i=0;i<arr.length;i++)
-{
-    console.log(arr[i])
+// promise.then()(res=>{console.log(res)}).catch(err=>{console.log(err)})
+
+console.log(13);
+console.log(1);
+console.log("Hi")
+function example(){
+    console.log("Rachu");
 }
-
-arr.push("Mayuthra")
-console.log(arr)
-arr.pop()
-console.log(arr)
-
-function example()
-{
-    console.log("Hello everyone this is my js")
-}example()
-
-const ptag=document.getElementById("ptag")
-ptag.innerHTML ="Manthravarthini.A.P"
-
-const input =document.getElementById("input")
-const  output =document.getElementById("inputField")
-input.addEventListener("input",()=>{
-    output.textContent=input.value})
-
-
-    const first=document.createElement("h")
-    first.textContent="Helloooo"
-    document.body.appendChild(first)
-
-    
+const example1 = ()=>{
+    const a=1;
+    const b=2;
+    console.log(a+b);
+}
+setTimeout(example,3000);
+setTimeout(example1,4500);
